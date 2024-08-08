42f21574843a8937a5cc2da26361f3858f4dd843
CodeAaronM <aaron.martinez.b@gmail.com>
2024-08-08 18:17:56 +0000

    se culmino el proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import { checkPropTypes } from "prop-types";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+const [ColorGreen, setColorGreen]= useState("opacity-50")
+const [ColorYellow, setColorYellow]= useState("opacity-50")
+const [ColorRed, setColorRed]= useState("opacity-50")
+
+
+
+function onGreen(){
+		setColorGreen("selected3")
+		setColorYellow("opacity-50")
+		setColorRed("opacity-50")
+}
+
+
+function onYellow(){
+	setColorGreen("opacity-50")
+	setColorYellow("selected2")
+	setColorRed("opacity-50")
+}
+
+
+function onRed(){
+	setColorGreen("opacity-50")
+	setColorYellow("opacity-50")
+	setColorRed("selected1")
+}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+<>
+<div className="container-fluid d-flex justify-content-center">
+<div className="container bg-dark row py-2 " style={{width: "124px",}}>
+<button type="button" className={`btn btn-danger col-12 align-self-center ${ColorRed}`} style={{width: "100px", height:"100px"}}  onClick={onRed}>
+</button>
+<button type="button" className={`btn btn-warning col-12 align-self-center ${ColorYellow}`} style={{width: "100px", height:"100px"}}  onClick={onYellow}>
+</button>
+<button type="button" className={`btn btn-success col-12 align-self-center ${ColorGreen}`} style={{width: "100px", height:"100px"}}  onClick={onGreen}>
+</button>
+</div>
+</div>
+</>
 	);
 };
 
